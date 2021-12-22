9c5b2e1b1044b45072f5158a076c54fe26639fa3
raiyan22 <raiyanislam058@gmail.com>
2021-12-23 00:43:08 +0600

    Homepage updated

--- Tshop/Areas/Customer/Controllers/HomeController.cs
+++ Tshop/Areas/Customer/Controllers/HomeController.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Tshop.Data;
 using Tshop.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 
 namespace Tshop.Controllers
 {
@@ -13,15 +18,19 @@ namespace Tshop.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext db, ILogger<HomeController> logger)
         {
+            _db = db;
             _logger = logger;
         }
-
         public IActionResult Index()
         {
-            return View();
+            return View(_db.Products
+                                .Include(item => item.ProductTypes)
+                                    .Include(i => i.Size)
+                                           .ToList());
         }
 
         public IActionResult Privacy()
