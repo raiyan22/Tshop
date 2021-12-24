dd65390f3d952884d18c2e9e437aa4be58b9d651
raiyan22 <raiyanislam058@gmail.com>
2021-12-24 19:33:02 +0600

    working on checkout.cshtml

--- Tshop/Areas/Customer/Controllers/OrderController.cs
+++ Tshop/Areas/Customer/Controllers/OrderController.cs
@@ -8,21 +8,58 @@ using Tshop.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Tshop.Utility;
 
 namespace Tshop.Areas.Customer.Controllers
 {
+    [Area("Customer")]
     public class OrderController : Controller
     {
-
         private ApplicationDbContext _db;
 
         public OrderController(ApplicationDbContext db)
         {
             _db = db;
         }
-        public IActionResult Index()
+
+        //GET Checkout action method
+
+        public IActionResult Checkout()
         {
             return View();
         }
+
+        //POST Checkout action method
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Checkout(Order anOrder)
+        {
+            List<Products> products = HttpContext.Session.Get<List<Products>>("products");
+            if (products != null)
+            {
+                foreach (var product in products)
+                {
+                    OrderDetails orderDetails = new OrderDetails();
+                    orderDetails.PorductId = product.Id;
+                    anOrder.OrderDetails.Add(orderDetails);
+                }
+            }
+
+            anOrder.OrderNo = GetOrderNo();
+            _db.Orders.Add(anOrder);
+            await _db.SaveChangesAsync();
+            HttpContext.Session.Set("products", new List<Products>());
+            return View();
+        }
+
+
+        public string GetOrderNo()
+        {
+            int rowCount = _db.Orders.ToList().Count() + 1;
+            return rowCount.ToString("000");
+        }
+
     }
 }
